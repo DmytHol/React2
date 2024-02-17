c184f8e36c6230592f5365c86bd9c50644602f09
DmytHol <dmytroholovniapro@gmail.com>
2024-02-16 23:53:26 -0500

    fixed bugs in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import { aboutMe } from "./data.js";
 import TextAboutMe from "./components/TextAboutMe";
-import Button from "./components/Button.jsx";
+import Button from "./components/button/Button.jsx";
 
 function App() {
   function handleClick(type) {
@@ -15,22 +15,22 @@ function App() {
         <section>
           <h2>Dmytro</h2>
           <ul>
-            <TextAboutMe title={aboutMe[0].title} description={aboutMe[0].description}/>
+            <TextAboutMe
+              title={aboutMe[0].title}
+              description={aboutMe[0].description}
+            />
             <TextAboutMe {...aboutMe[1]} />
             <TextAboutMe {...aboutMe[2]} />
             <TextAboutMe {...aboutMe[3]} />
           </ul>
         </section>
         <section>
-          <h3></h3>
-          <h3>
-            <h3>Reach me</h3>
-          </h3>
-          <Button onClick={() => handleClick('linkedin')}>LinkedIn</Button>
-          <Button onClick={() => handleClick('github')}>GitHub</Button>
-          <Button onClick={() => handleClick('facebook')}>Facebook</Button>
+          <h3>Reach me at: </h3>
+
+          <Button onClick={() => handleClick("linkedin")}>LinkedIn</Button>
+          <Button onClick={() => handleClick("github")}>GitHub</Button>
+          <Button onClick={() => handleClick("facebook")}>Facebook</Button>
         </section>
-        
       </main>
     </div>
   );
